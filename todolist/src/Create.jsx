97219ffad6fc15cc8d8ac1a12e0d77fa833aa2ac
PRAKASH THAPA <prakasambhav24@gmail.com>
2024-12-18 16:47:12 +0545

    adding express api url to add logic..

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,23 +1,30 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function Create() {
-    const [task,setTask] = useState()
-    const handleAdd = ()=>{
-        axios.post('http://localhost:3001/add',{task:task})
-        .then(result => {
-          location.reload()
-        })
-        .catch(err => console.log(err))
-    }
+  const [task, setTask] = useState();
+  const handleAdd = () => {
+    axios
+      .post("https://vercel-todolist-api.vercel.app/add", { task: task })
+      .then((result) => {
+        location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className='create_form'>
-        <input type="text" placeholder='Enter Task' onChange={(e)=> setTask(e.target.value)}/>
-        <button type="button" onClick={handleAdd}>Add</button>
+    <div className="create_form">
+      <input
+        type="text"
+        placeholder="Enter Task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Create;
