ee7f3d9bdb4da90ba9f175f2995e1b207d9a3245
PRAKASH THAPA <prakasambhav24@gmail.com>
2024-12-18 16:05:16 +0545

    adding .env

--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const TodoModel = require("./Models/Todo");
+const { config } = require("dotenv");
+config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/test");
+mongoose.connect(process.env.MONGODB_URL);
 
 app.get("/get", (req, res) => {
   TodoModel.find()
